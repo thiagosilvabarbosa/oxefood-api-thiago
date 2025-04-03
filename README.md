56fdb5758db683b8fb5deedd7e916eecf8c03ad8
Thiago Barbosa <contatobarbosathiago@gmail.com>
2025-04-03 20:53:41 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # oxefood-api-thiago
 Projeto Utilizado para disciplina de web4 do IFPE Campus Jaboatão
+./mvnw spring-boot:run - iniciar o projeto
---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiThiagoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiThiagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiThiagoApplication.class, args);
+	}
+
+}
