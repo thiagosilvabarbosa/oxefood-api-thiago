6870884c8a6281cd0435818aab822f3a438ed4d0
Thiago Barbosa <contatobarbosathiago@gmail.com>
2025-05-02 15:33:33 -0300

    Add: Entidade Entregador

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -0,0 +1,20 @@
+package br.com.ifpe.oxefood.modelo.entregador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class EntregadorService {
+
+    @Autowired
+    private EntregadorRepository repository;
+
+    @Transactional
+    public Entregador save(Entregador entregador){
+        entregador.setHabilitado(Boolean.TRUE);
+        return repository.save(entregador);
+    }
+
+}
